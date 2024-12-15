3bdb406e560833814107d244d60b01a7217aaf50
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2024-12-15 19:12:14 +0530

    params vs arguments

---
+++ functions/paramsVsarguments.js
@@ -0,0 +1,16 @@
+/*
+
+q=>     PARAMS VS ARGUMENTS
+
+ANS --> When We Pass a value inside a function is called ARGUMETS , but when we recieve that value in a function we call it as a PARAMS 
+*/
+
+
+// function square(num) { // ---> Params
+//     console.log(num*num)
+// }
+
+// square(10) // ---> Argumet
+
+
+
