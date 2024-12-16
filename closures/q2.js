e3a1d305160c096668d2a0d003f6f1cfbff17061
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2024-12-16 13:00:35 +0530

    closures

---
+++ closures/q2.js
@@ -0,0 +1,16 @@
+/*
+ q2 WRITE A FUNCTION THAT WOULD ALLOW YOU TO DO THIS
+
+ var addSix = createBase(6);
+addSix(10) -> 16
+addSix(20) -> 26
+
+*/
+
+var addSix = createBase(6){
+    return function(a){
+        return a+6;
+
+    }
+}
+
